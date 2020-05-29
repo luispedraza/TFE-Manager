b85097e52cb5d7b367e231ee2e4b8d0fcbfe2c9a
Luis Pedraza <luispedraza@gmail.com>
2020-05-29 16:40:49 +0200

    Proyecto creado y ventana principal con algunas funciones básicas

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.*;
+
+
+
+public class MainWindow extends JDialog {
+    static final String WORKING_DIRECTORY = "/Users/luispedraza/OneDrive - Universidad Internacional de La Rioja/TFE-MANAGER";
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JButton unzipProposals;
+    private JButton loadProposals;
+    private JTextArea logTextArea;
+
+    public MainWindow() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        unzipProposals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                unzipProposals();
+            }
+        });
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    private void unzipProposals() {
+        final JFileChooser fc = new JFileChooser(MainWindow.WORKING_DIRECTORY);
+        fc.setFileFilter(new FileNameExtensionFilter("Archivo zip", "zip"));
+        int returnVal = fc.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            String fileName = fc.getSelectedFile().getName();
+            logInfo("Descromprimiendo el archivo de propuestas: " + fileName);
+        }
+    }
+
+    private void logInfo(String info) {
+        logTextArea.setText(
+                logTextArea.getText() + "\n" + info
+        );
+    }
+
+    public static void main(String[] args) {
+        MainWindow dialog = new MainWindow();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}

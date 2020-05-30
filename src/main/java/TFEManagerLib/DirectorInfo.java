436f8610dd6636deab58e92d13884b2c73ac0e9c
Luis Pedraza <luispedraza@gmail.com>
2020-05-30 20:51:51 +0200

    agregadas clases para directores y revisores

---
+++ src/main/java/TFEManagerLib/DirectorInfo.java
@@ -0,0 +1,9 @@
+package TFEManagerLib;
+
+import java.util.HashMap;
+
+/** Clase para almacenar la información de un Director
+ * 
+ */
+public class DirectorInfo extends HashMap<String, String> {
+}
---
+++ src/main/java/TFEManagerLib/ReviewerInfo.java
@@ -0,0 +1,11 @@
+package TFEManagerLib;
+
+
+import java.util.HashMap;
+
+/**
+ * Clase para almacenar la información de un revisor
+ *
+ */
+public class ReviewerInfo extends HashMap<String, String> {
+}

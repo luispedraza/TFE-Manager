ca674783acefbfc96bf65df41bf4ff08a9a39b9e
Luis Pedraza <luispedraza@gmail.com>
2020-07-29 16:40:13 +0200

    clase group

--- src/main/java/TFEManagerLib/Models/Group.java
+++ src/main/java/TFEManagerLib/Models/Group.java
@@ -1,9 +1,11 @@
 package TFEManagerLib.Models;
 
-/** Clase de grupo que extiende la clase de alumno
+/** Clase de grupo que extiende la clase de académico
+ * Podemos considearrlos un contenedor de alumnos, del mismo modo que
+ * un revisor contiene alumnos a revisar, y un director alumnos a dirigir
  *
  */
-public class Group extends Student {
+public class Group extends Academic {
 
 
 }
